91a4a60c14d1cb5da6af2969e38d9fef20134e2a
Saku Suorsa <suorsa.saku@gmail.com>
2022-06-05 22:17:36 +0300

    aloitin maatehtävää

---
+++ osa2/maidentiedot/src/App.js
@@ -0,0 +1,104 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Input = (props) => {
+  const handleChange = (event) => {
+    props.change(event.target.value)
+  }
+
+  return (
+    <div>
+      {props.text}<input 
+        value={props.value}
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
+
+const Country = (props) => {
+  const country = props.country
+  const languages = Object.values(country.languages)
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+
+      <div>
+        capital {country.capital}
+      </div>
+      <div>
+        area {country.area}
+      </div>
+
+      <p>
+        <b>languages: </b>
+      </p>
+
+      <div>
+        <ul>
+          {languages.map(l =>
+            <li key={l}>
+              {l}
+            </li>
+          )}
+        </ul>
+      </div>
+
+      <p>
+        <img src={country.flags.png} />
+      </p>
+    </div>
+  )
+}
+
+const Countries = (props) => {
+  const countries = props.countries
+  const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(props.filter.toLowerCase()))
+
+  if(countriesToShow.length > 10) {
+    return (
+    <div>
+      Too many matches, specify another filter
+    </div>)
+  } else if(countriesToShow.length == 1) {
+    return (
+      <Country country={countriesToShow[0]}/>
+    )
+  } else {
+    return (
+      <div>
+        {countriesToShow.map(country =>
+          <div key={country.name.common}>
+            {country.name.common}
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+  console.log('render', countries.length, 'countries')
+
+  return (
+    <div>
+    <Input text={'find countries'} change={setFilter} />
+    <Countries countries={countries} filter={filter}/>
+    </div>
+  )
+}
+
+export default App;
